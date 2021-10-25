1b4f1bf5aa5301b57412cf5765d5beddeac2ed03
sumi <ninzin60en.dev@gmail.com>
2021-10-25 22:49:27 +0900

    Initial Commit

---
+++ MyCoffeeBook/MyCoffeeBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCoffeeBookApp.swift
+//  MyCoffeeBook
+//
+//  Created by nekuro on 2021/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyCoffeeBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
